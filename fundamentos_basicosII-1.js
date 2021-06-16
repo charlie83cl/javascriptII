8b8ca391bb88ad079a35ac187c84e23691561605
charlie83cl <82404370+charlie83cl@users.noreply.github.com>
2021-06-16 15:19:17 -0400

    agregue nuevas respuestas a fundamentos basicos java 2

--- fundamentos_basicosII-1.js
+++ fundamentos_basicosII-1.js
@@ -40,6 +40,55 @@ arreglo =[5,15,1];
 console.log(MayorMenor(arreglo));
 
 /*
+3. Imprime (print) uno, devuelve (return) 
+otro- Crea una función para un array de números.
+La función debería imprimir (print) el penúltimo valor y 
+devolver (return) el primer valor impar.
+*/
+function imprunodevotro(arr){
+    console.log(arr[arr.length-2]);
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] % 2 == 1){
+            return arr[i];
+        }
+    }
+}
+console.log(imprunodevotro([0,2,1,4,3]));
+
+/*
+4. Doble Visión - Dado un array, 
+crea una función que devuelva un nuevo array 
+donde cada valor se duplique. Entonces, doble([1,2,3]) 
+debiera devolver [2, 4, 6] sin cambiar el array original. 
+*/
+function dobleVision(arr){
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++){
+        newArr.push(arr[i] * 2);
+    }
+    return newArr;
+}
+    console.log(dobleVision([1,2,3]));
+
+/*
+5. Contar Positivos -  Dado un array de números, 
+crea una función para reemplazar el último valor 
+con el número de valores positivos encontrados en el array. 
+Ejemplo, contarPositivos([-1,1,1,1]) cambia el array original y devuelve [-1,1,1,3].
+*/
+function countPositives(arr){
+    var positives = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] > 0){
+            positives++;
+        }
+    }
+    arr[arr.length-1] = positives;
+    return arr;
+}
+console.log(countPositives([1,1,0,0]));
+
+/*
 6 Imprime (print) uno, devuelve (return) otro- Crea una función para un array de números.
  La función debería imprimir (print) el penúltimo valor y devolver (return) el primer valor impar.
 */
@@ -179,7 +228,8 @@ hambre([1,2,4,"hola"]);
 /*
 13.  Cambiar hacia el centro -  Dado un array, 
 cambia el primer y último valor, el tercero 
-con el ante penútimo, etc. Ejemplo: 
+con el ante penúltimo, etc. 
+Ejemplo: 
 cambiaHaciaElCentro([true, 42, “Ada”, 2, “pizza”]) 
 cambia el array a [“pizza¨, 42, “Ada”, 2, true]. 
 cambiaHaciaElCentro([1,2,3,4,5,6]) cambia el array a [6,2,4,3,5,1]. 
@@ -196,6 +246,9 @@ function swapTowardCenter(arr){
 }
 
 console.log(swapTowardCenter([1,2,3,4,5,6]));
+
+
+
 
 /*
 14. Escala el Array - Dado un array arr y un número num, 
